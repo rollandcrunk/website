d3143b38bb74d027c026fb4d28e23b0e1919b502
Rolland Crunk <rcrunk@yahoo.com>
2016-08-04 19:41:39 -0700

    Arranged so navigation dropdown collapses on route change.

--- app/components/navbar.component.ts
+++ app/components/navbar.component.ts
@@ -1,10 +1,24 @@
-import { Component } from "@angular/core";
-import { ROUTER_DIRECTIVES } from "@angular/router";
+import { Component, ViewChild, ElementRef } from "@angular/core";
+import { Router, Event, NavigationStart, ROUTER_DIRECTIVES } from "@angular/router";
+
+import {LogoComponent} from "./logo.component";
+
 
 @Component({
   selector: 'rc-navbar',
   templateUrl: 'app/components/navbar.component.html',
-  directives: [ROUTER_DIRECTIVES]
+  directives: [LogoComponent, ROUTER_DIRECTIVES]
 })
 export class NavbarComponent {
+  @ViewChild('menuToggle') navbarElement: ElementRef;
+
+  constructor(private router: Router) {
+    router.events.subscribe((e) => this.handleRouteChange(e));
+  }
+
+  private handleRouteChange = (event: Event) => {
+    if (event instanceof NavigationStart && this.navbarElement) {
+      this.navbarElement.nativeElement.className += 'collapse';
+    }
+  }
 }
