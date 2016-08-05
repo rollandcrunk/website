16566fccc971a94fddaca0c5b7536cb5d241fbb4
Rolland Crunk <rcrunk@yahoo.com>
2016-08-04 19:49:32 -0700

    Gussied up the 404 page a tiny bit.

--- app/components/not-found.component.ts
+++ app/components/not-found.component.ts
@@ -7,9 +7,8 @@ import {Component} from "@angular/core";
       <div class="page-header m-t-1">
         <h2>Uh oh</h2>
       </div>
-      <p>
-        You've stumbled across an unknown route; undoubtedly through no fault of your own.
-      </p>
+      <p class="lead">You've stumbled across an unknown route; undoubtedly through no fault of your own.</p>
+      <p>Click one of the juvawavis above to get to the good stuff.</p>
     </div>
   `
 })
