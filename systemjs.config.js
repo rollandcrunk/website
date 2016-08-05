b95c78b96f08d53f07cedc9e393c8fd6cf9c13fb
Rolland Crunk <rcrunk@yahoo.com>
2016-08-04 19:39:45 -0700

    Fixed barrel resolution for components.

--- systemjs.config.js
+++ systemjs.config.js
@@ -10,13 +10,14 @@
     'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
     'rxjs':                       'node_modules/rxjs',
 
-    'components':                 'app/components/index.js'
+    'components':                 'app/components'
   };
   // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
     'app':                        { main: 'main.js',  defaultExtension: 'js' },
     'rxjs':                       { defaultExtension: 'js' },
     'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
+    'components':                 { main: 'index.js', defaultExtension: 'js' },
   };
   var ngPackageNames = [
     'common',
@@ -30,7 +31,7 @@
     'router-deprecated',
     'upgrade',
 
-    'components'
+    //'components'
   ];
   // Individual files (~300 requests):
   function packIndex(pkgName) {
