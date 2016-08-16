cb7e17c254d5554d0efb36266d82dab91dfe9d47
Rolland Crunk <rcrunk@yahoo.com>
2016-08-16 01:32:25 -0700

    Removed link and experimental code dealing with animate.css. Changed icons for expansion/contraction of bullet lists.

--- app/components/about.component.ts
+++ app/components/about.component.ts
@@ -3,8 +3,8 @@ import {Component, ChangeDetectorRef, AfterViewInit, Renderer, ViewEncapsulation
 import {ActivatedRoute} from "@angular/router";
 
 import {PageAnimation} from "./shared/page.animation";
-import {CarouselComponent, CarouselMedia} from "./carousel.component";
 import {MemoirService} from "../services/memoir.service";
+import {CarouselComponent, CarouselMedia} from "./carousel.component";
 
 const expandedClassName = 'rc-expanded';
 
@@ -45,7 +45,6 @@ export class AboutComponent implements AfterViewInit {
   }
 
   expand(event: any): void {
-    console.log("expanded: " + event.target);
     if (event.target instanceof HTMLLIElement) {
       let noteItem = event.target;
       let oldNoteItem = this.selectedNote;
@@ -58,51 +57,10 @@ export class AboutComponent implements AfterViewInit {
         this.renderer.setElementClass(this.selectedNote, expandedClassName, true);
       }
       else this.selectedNote = noteItem = null;
-
-      this.playMediaAnimation(oldNoteItem, noteItem);
     }
   }
 
   memoir(title: string): CarouselMedia[] {
     return this.memoirService.getMemoir(title);
-  }
-
-  private playMediaAnimation(deactivated: HTMLLIElement, activated: HTMLLIElement) {
-    if (deactivated) {
-      let carouselElements = deactivated.getElementsByClassName('rc-media');
-      if (carouselElements) {
-        for (let i = 0; i < carouselElements.length; ++i) {
-          this.deactivate(carouselElements[i])
-              .then(() => {
-                this.setElementClasses(carouselElements[i], false, 'zoomOutLeft', 'animate');
-                this.playMediaAnimation(null, activated);
-              });
-        }
-      }
-    }
-
-    if (activated) {
-      let carouselElements = activated.getElementsByClassName('rc-media');
-      if (carouselElements) {
-        for (let i = 0; i < carouselElements.length; ++i) {
-          this.activate(carouselElements[i])
-            .then(() => this.setElementClasses(carouselElements[i], false, 'zoomInRight', 'animate'));
-        }
-      }
-    }
-  }
-
-  private deactivate = (element: Element): Promise<any> => {
-    this.setElementClasses(element, true, 'zoomOutLeft', 'animate');
-    return new Promise((fulfill) => setTimeout(fulfill, 500));
-  };
-
-  private activate = (element: Element): Promise<any> => {
-    this.setElementClasses(element, true, 'zoomInRight', 'animate');
-    return new Promise((fulfill) => setTimeout(fulfill, 500));
-  };
-
-  private setElementClasses = (element: Element, state: boolean, ...names: string[]): void => {
-    for (let i = 0; i < names.length; ++i) this.renderer.setElementClass(element, names[i], state);
   }
 }
