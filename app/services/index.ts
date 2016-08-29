ee723423d64cc7759bb1d957c30137de888dd4e5
Rolland Crunk <rcrunk@yahoo.com>
2016-08-28 23:44:55 -0700

    Updated tether.js version.

--- app/services/index.ts
+++ app/services/index.ts
@@ -1,3 +1,5 @@
+export * from "./experience.service";
 export * from "./memoir.service";
 export * from "./observation.service";
 export * from "./quote.service";
+export * from "./tooltip.service";
