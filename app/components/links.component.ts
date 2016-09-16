ec7b52374a97ce4f4c185d4ca2b1d01658b47686
Rolland Crunk <rcrunk@yahoo.com>
2016-09-16 00:09:18 -0700

    Release candidate for links.

--- app/components/links.component.ts
+++ app/components/links.component.ts
@@ -15,12 +15,44 @@ import {PageAnimation} from "./shared/page.animation";
         Other websites of mine.
         <ul>
           <li>
-            <a href="http://crunk.net">Crunk.net</a> - Where it all started; my first website.
+            <a href="http://crunk.net" target="_blank">Crunk.net</a> - My first personal website.
             All of the original content was taken down for reconstruction and
-            never reappeared. Instead being replaced with a CSS theme demo, originally
-            built using Ruby on Rails, the server side was later deemed unneeded and
-            redone simply using client javascript; a later (and current) permutation 
-            utilizes Angular Dart.
+            never reappeared. Instead, being replaced with a CSS themes demo that gets a refresh every
+            few years when its technology gets stale and there is new technology that I want to play with.
+            The current permutation is written using Angular Dart.
+          </li>
+        </ul>
+        <p>
+        User facing commercial sites I've helped build:
+        <ul>
+          <li>
+            <a href="https://www.google.com/about/careers/jobs" target="_blank">Google Careers</a>
+            A great place to work. The original site is still <a href="https://www.google.com/about/careers/search" target="_blank">here</a>.
+          </li>
+        </ul>
+        <p>
+          Sites I frequent.
+        <ul>
+          <li>
+            <a href="http://reason.com/" target="_blank">Reason Magazine</a> 
+            - For news, information, and opinion that is in rough alignment with my own.
+          </li>
+          <li>
+            <a href="http://antiwar.com/" target="_blank">AntiWar.COM</a> 
+            - A collection of news and information that generally goes under reported by main stream media. 
+          </li>
+          <li>
+            <a href="https://dzone.com/" target="_blank">DZone</a> 
+            - Technical articles relating to a broad range of topics in software development.
+          </li>
+          <li>
+            <a href="http://stackoverflow.com/" target="_blank">Stack Overflow</a> 
+            - My navigation here is typically from search results hits. An extraordinarily
+            valuable resource for software developers.
+          </li>
+          <li>
+            <a href="https://www.smashingmagazine.com/" target="_blank">Smashing Magazine</a> 
+            - Helpful articles covering different aspects of web design and development.
           </li>
         </ul>
     </div>
